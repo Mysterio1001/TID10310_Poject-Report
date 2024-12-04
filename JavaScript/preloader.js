cbf359e8fce5967b4ca093dc5a53b41429d6603e
Mysterio1001 <holmes9073@gmail.com>
2024-12-04 19:12:22 +0800

    index preloader ver.new

--- JavaScript/preloader.js
+++ JavaScript/preloader.js
@@ -1,6 +1,38 @@
 // 轉場動畫
+// window.addEventListener('load',()=>{
+//    if(window.innerWidth > 820){
+   
+//     let preloader = document.querySelector('#preloader');
+//     let body =  document.querySelector('body');
+ 
+//     body.classList.add('loaded');
+ 
+//     setTimeout(()=>{  
+//        preloader.classList.add('loaded'); 
+//        body.classList.remove('loaded'); 
+//     },1400) //1.4秒後消失
+ 
+//     let loading = document.querySelector('#loading');
+ 
+//     let IntervalItem = setInterval(()=>{
+//        loading.textContent += " ."   //Loading加上.號
+//     },400);
+ 
+//     setTimeout(()=>{
+//        clearInterval(IntervalItem);  //停止Interval
+//     },1200)
+//    }else{
+//     let preloader = document.querySelector('#preloader');
+//     preloader.style.display = "none";
+//    } 
+//  });
+
+
+
+ 
+
+
 window.addEventListener('load',()=>{
-   if(window.innerWidth > 820){
    
     let preloader = document.querySelector('#preloader');
     let body =  document.querySelector('body');
@@ -20,12 +52,7 @@ window.addEventListener('load',()=>{
  
     setTimeout(()=>{
        clearInterval(IntervalItem);  //停止Interval
-    },1200)
-   }else{
-    let preloader = document.querySelector('#preloader');
-    preloader.style.display = "none";
-   } 
+    },1400)
+   
+   
  });
-
-
- 
